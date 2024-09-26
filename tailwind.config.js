46df94862e0cf601c4a3cd3b2efd6d9f8a137ac8
JentleJoe <aigbokhaebholojoshua@gmail.com>
2024-09-26 17:50:24 +0100

    added custom colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.blue,
+        secondary: colors.yellow,
+        neutral: colors.gray,
+      }
+    },
   },
   plugins: [],
 }
