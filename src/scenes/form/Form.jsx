64472d6de6c1727648f7033834f12964efd7c477
JentleJoe <aigbokhaebholojoshua@gmail.com>
2024-10-19 14:37:11 +0100

    built form component

--- src/scenes/form/Form.jsx
+++ src/scenes/form/Form.jsx
@@ -1,9 +1,17 @@
 import { Formik } from "formik"
 import * as Yup from 'yup';
 import { Header } from "../../components"
+import { useMediaQuery } from "@mui/material";
+import {TextField} from "@mui/material";
+import {Box} from "@mui/material";
+import {Button} from "@mui/material";
+import { useTheme } from "../../theme/ThemeContext";
 
 
 const Form = () => {
+    const { theme, isDarkMode } = useTheme()
+
+    const isNonMobile = useMediaQuery("(min-width:600px)")
 
     const phoneRegExp = /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
 
@@ -32,6 +40,44 @@ const Form = () => {
         console.log(values)
     }
 
+    const colors = {
+        inputBackground: '#f2f0f0',
+        inputHoverBg: '#e6f3ff',
+        inputFocusBg: '#ffffff',
+        labelColor: '#2c3e50'
+    }
+
+    const typography = {
+        inputFontSize: '16px',
+        labelFontSize: '12px',
+        helperTextSize: '12px'
+    }
+
+    const textFieldStyles = {
+        '& .MuiFilledInput-root': {
+            backgroundColor: colors.inputBackground,
+            '&:hover': {
+                backgroundColor: colors.inputHoverBg
+            },
+            '&.Mui-focused': {
+                backgroundColor: colors.inputFocusBg
+            }
+        },
+        '& .MuiFilledInput-input': {
+            fontSize: typography.inputFontSize,
+        },
+        '& .MuiInputLabel-root': {
+            fontSize: typography.labelFontSize,
+            color: colors.labelColor,
+            '&.Mui-focused': {
+                color: colors.labelColor
+            }
+        },
+        '& .MuiFormHelperText-root': {
+            fontSize: typography.helperTextSize
+        }
+    }
+
   return (
     <div>
         <Header title="CREATE USER" subtitle="Create a New User Profile" />
@@ -41,9 +87,118 @@ const Form = () => {
             validationSchema={userSchema}
         >
             {({ values, errors, touched, handleBlur, handleChange, handleSubmit }) => (
-                <form className="grid gap-8 grid-cols-4 ">
-
-                </form>
+                <form onSubmit={handleSubmit}>
+                <Box
+                  display="grid"
+                  gap="30px"
+                  gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+                  sx={{
+                    "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
+                  }}
+                >
+                  <TextField
+                    fullWidth
+                    variant="filled"
+                    type="text"
+                    label="First Name"
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    value={values.firstName}
+                    name="firstName"
+                    error={!!touched.firstName && !!errors.firstName}
+                    helperText={touched.firstName && errors.firstName}
+                    sx={{ 
+                        gridColumn: "span 2",
+                        ...textFieldStyles,
+                    }}
+                  />
+                  <TextField
+                    fullWidth
+                    variant="filled"
+                    type="text"
+                    label="Last Name"
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    value={values.lastName}
+                    name="lastName"
+                    error={!!touched.lastName && !!errors.lastName}
+                    helperText={touched.lastName && errors.lastName}
+                    sx={{ 
+                        gridColumn: "span 2",
+                        ...textFieldStyles,
+                    }}
+                  />
+                  <TextField
+                    fullWidth
+                    variant="filled"
+                    type="text"
+                    label="Email"
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    value={values.email}
+                    name="email"
+                    error={!!touched.email && !!errors.email}
+                    helperText={touched.email && errors.email}
+                    sx={{ 
+                        gridColumn: "span 4",
+                        ...textFieldStyles,
+                    }}
+                  />
+                  <TextField
+                    fullWidth
+                    variant="filled"
+                    type="text"
+                    label="Contact Number"
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    value={values.contact}
+                    name="contact"
+                    error={!!touched.contact && !!errors.contact}
+                    helperText={touched.contact && errors.contact}
+                    sx={{ 
+                        gridColumn: "span 4",
+                        ...textFieldStyles,
+                    }}
+                  />
+                  <TextField
+                    fullWidth
+                    variant="filled"
+                    type="text"
+                    label="Address 1"
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    value={values.address1}
+                    name="address1"
+                    error={!!touched.address1 && !!errors.address1}
+                    helperText={touched.address1 && errors.address1}
+                    sx={{ 
+                        gridColumn: "span 4",
+                        ...textFieldStyles,
+                    }}
+                  />
+                  <TextField
+                    fullWidth
+                    variant="filled"
+                    type="text"
+                    label="Address 2"
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    value={values.address2}
+                    name="address2"
+                    error={!!touched.address2 && !!errors.address2}
+                    helperText={touched.address2 && errors.address2}
+                    sx={{ 
+                        gridColumn: "span 4",
+                        ...textFieldStyles,
+                    }}
+                  />
+                </Box>
+                <Box display="flex" justifyContent="end" mt="20px">
+                  <Button type="submit" color="secondary" variant="contained">
+                    Create New User
+                  </Button>
+                </Box>
+              </form>
             )}
         </Formik>
     </div>
