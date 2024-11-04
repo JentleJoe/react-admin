63628a0db16c0f8383f5ebf013747e37707493ae
JentleJoe <aigbokhaebholojoshua@gmail.com>
2024-11-04 12:29:31 +0100

    udpated faq questions

--- src/scenes/faq/Faq.jsx
+++ src/scenes/faq/Faq.jsx
@@ -9,28 +9,31 @@ const Faq = () => {
    const faqs = [
     {
         question: 'What is an Admin Dashboard',
-        answer: `Lorem ipsum dolor sit amet, consetetur adipiscing elit. Suspendisse
-                malesuada lacus ex, sit amet blandit leo lobortis eget`,
+        answer: `An admin dashboard is like a control center for managing a website or app.
+        It helps you track important info, make changes, and keep things running smoothly all
+        in one place without having to write any code!`,
     },
     {
         question: 'What is JavaScript',
-        answer: `Lorem ipsum dolor sit amet, consetetur adipiscing elit. Suspendisse
-                malesuada lacus ex, sit amet blandit leo lobortis eget`,
+        answer: `JavaScript is a programming language that helps make websites interactive.
+        It is what lets buttons, forms, animations, and other parts of a website respond when
+        you click, type, or scroll. JavaScript brings websites to life!`,
     },
     {
         question: 'What is React',
-        answer: `Lorem ipsum dolor sit amet, consetetur adipiscing elit. Suspendisse
-                malesuada lacus ex, sit amet blandit leo lobortis eget`,
+        answer: `React is a JavaScript framework. It is used to build websites
+        or applications that updates automatically without having to refresh the whole page`,
     },
     {
-        question: 'Who are you',
-        answer: `Lorem ipsum dolor sit amet, consetetur adipiscing elit. Suspendisse
-                malesuada lacus ex, sit amet blandit leo lobortis eget`,
+        question: 'What is a Framework',
+        answer: `A  framework is a collection of ready-made tools and rules that helps developers
+        build software more quickly and easily`,
     },
     {
-        question: 'What are you skills',
-        answer: `Lorem ipsum dolor sit amet, consetetur adipiscing elit. Suspendisse
-                malesuada lacus ex, sit amet blandit leo lobortis eget`,
+        question: 'What are Dependencies',
+        answer: `Dependencies are the extra tools or packages that a project depends on to work
+        properly. Using dependencies prevents you from having to build everything from scratch by
+        using ready-made tools built by other developers.`,
     },
    ]
 
@@ -40,7 +43,7 @@ const Faq = () => {
         {
             faqs.map((faq, index) => (
                 <div key={index} className="mb-[1px]">
-                    <Accordion defaultExpanded sx={theme.accordion} key={index}>
+                    <Accordion sx={theme.accordion} key={index}>
                         <AccordionSummary expandIcon={<ExpandMore />} 
                         sx={theme.accordionSummary}>
                             <h5 className={` text-[#3B9F8A]`}>{faq.question}</h5>
