1fca7fc46914cd4355a6b9d5c0a2aa281b5f467c
JentleJoe <aigbokhaebholojoshua@gmail.com>
2024-12-14 02:45:48 +0100

    completed dashboard

--- src/components/StatBox.jsx
+++ src/components/StatBox.jsx
@@ -2,18 +2,21 @@ import { useTheme } from "../theme/ThemeContext"
 import ProgressCircle from "./ProgressCircle"
 
 const StatBox = ({ title, value, icon, progress, increase  }) => {
+    const { theme } = useTheme()
+    const colors = theme.palette
+
   return (
     <div className="flex justify-between items-center h-full ">
         <div className="flex flex-col ">
-            <div className="stat-icon" >{icon}</div>
-            <h3 className="text-xl font-bold" >{value}</h3>
-            <p>{title}</p>
+            <div className="stat-icon mb-1" >{icon}</div>
+            <h3 className={`text-xl font-bold ${theme.text}`} >{value}</h3>
+            <p className={`text-[#4cc2ac]`}>{title}</p>
         </div>
         <div className="flex flex-col">
             <div>
                 <ProgressCircle progress={progress} size={45} strokeWidth={5} />
             </div>
-            <div className="font-">{increase}</div>
+            <div className={`text-[#4cc2ac] italic`}>{increase}</div>
         </div>
     </div>
   )
